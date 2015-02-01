0973fc1b14a99accf63e7bed21bb0e8ae7dc417a
sacgarg <satch326@yahoo.com>
2015-02-01 00:14:25 -0800

    Update tmate-bootstrap.go

--- tmate-bootstrap.go
+++ tmate-bootstrap.go
@@ -89,8 +89,8 @@ func main() {
 	log_action("tmate_bin:")
 	log.Print("tmate_bin =====> " + tmate_bin)
 	tmate_cmd := exec.Command(tmate_bin)
-	
-	os.Stdout.Write(tmate_cmd)
+	log_action("after tmate_cmd")
+	//os.Stdout.Write(tmate_cmd)
 	f, err := pty.Start(tmate_cmd)
 
 	if err != nil {
